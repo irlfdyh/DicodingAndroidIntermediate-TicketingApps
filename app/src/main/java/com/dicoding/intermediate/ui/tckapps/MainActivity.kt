620fe6a4e7f1931fbdb3a4bb5867f950122ce2d7
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-03 14:36:58 +0700

    add seats view to main activity

--- app/src/main/java/com/dicoding/intermediate/ui/tckapps/MainActivity.kt
+++ app/src/main/java/com/dicoding/intermediate/ui/tckapps/MainActivity.kt
@@ -2,10 +2,25 @@ package com.dicoding.intermediate.ui.tckapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seatsView = findViewById<SeatsView>(R.id.seats_view)
+        val button = findViewById<Button>(R.id.finish_button)
+
+        button.setOnClickListener {
+            seatsView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silahkan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
 }
